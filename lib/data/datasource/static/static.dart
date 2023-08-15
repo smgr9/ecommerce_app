23589e1f52c821543891bb99443b9244d0c05df3
smgr9 <mohamadgazer@gmail.com>
2023-08-15 15:53:51 +0300

    add image assets and root image in class

---
+++ lib/data/datasource/static/static.dart
@@ -0,0 +1,10 @@
+import 'package:ecommerce_course/data/model/onboardingmodel.dart';
+
+List<OnboardingModel> onboardingList = [
+  OnboardingModel(
+    title: "choose product",
+    image: ,
+    body:
+        "we Haer a 100k prouduct, choose \n your product from our E-commerce shop",
+  )
+];
