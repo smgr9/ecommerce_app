e50dff288432f26883245d4b718cc6901f0ba4b9
smgr9 <mohamadgazer@gmail.com>
2023-08-15 15:55:35 +0300

    start in statric data

---
+++ lib/core/constant/image_assets.dart
@@ -0,0 +1,7 @@
+class ImageAssets {
+  static const String rootimages = "assets/images";
+  static const String onBoardingImageOne = "$rootimages/onboardingone.PNG";
+  static const String onBoardingImageTwo = "$rootimages/onboardingtwo.PNG";
+  static const String onBoardingImageThree = "$rootimages/onboardingthree.PNG";
+  static const String onBoardingImageFour = "$rootimages/onboardingfour.PNG";
+}
--- lib/data/datasource/static/static.dart
+++ lib/data/datasource/static/static.dart
@@ -1,10 +1,29 @@
+import 'package:ecommerce_course/core/constant/image_assets.dart';
 import 'package:ecommerce_course/data/model/onboardingmodel.dart';
 
 List<OnboardingModel> onboardingList = [
   OnboardingModel(
     title: "choose product",
-    image: ,
+    image: ImageAssets.onBoardingImageOne,
     body:
         "we Haer a 100k prouduct, choose \n your product from our E-commerce shop",
-  )
+  ),
+  OnboardingModel(
+    title: "choose product",
+    image: ImageAssets.onBoardingImageTwo,
+    body:
+        "we Haer a 100k prouduct, choose \n your product from our E-commerce shop",
+  ),
+  OnboardingModel(
+    title: "choose product",
+    image: ImageAssets.onBoardingImageThree,
+    body:
+        "we Haer a 100k prouduct, choose \n your product from our E-commerce shop",
+  ),
+  OnboardingModel(
+    title: "choose product",
+    image: ImageAssets.onBoardingImageFour,
+    body:
+        "we Haer a 100k prouduct, choose \n your product from our E-commerce shop",
+  ),
 ];
